a2c8f14ef341620627324735605fdf190733cff9
NZhukov <nzhukov.rnd@gmail.com>
2023-07-10 09:30:50 +0400

    add model secretValue

--- Right on target/Model/SecretValue.swift
+++ Right on target/Model/SecretValue.swift
@@ -12,4 +12,25 @@ typealias SecretColorValue = SecretValue<Color>
 
 protocol SecretValueProtocol {
     associatedtype ValueType
+    
+    var value: ValueType { get }
+    
+    mutating func setRandomValue()
+}
+
+struct SecretValue<T: Equatable>: SecretValueProtocol {
+    typealias ValueType = T
+    var value: T
+    
+    private let randomValueClosure: (T) -> T
+    init(initValue: T, randomValueClosure: @escaping (T) -> T) {
+        value = initValue
+        self.randomValueClosure = randomValueClosure
+    }
+    
+    mutating func setRandomValue() {
+        self.value = randomValueClosure(self.value)
+    }
+    
+    
 }
